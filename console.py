9c91e27c70d5630fbfa4d7b9a09d945b4405d13d
Balvine <balvy075@gmail.com>
2022-09-02 16:47:12 +0300

    intro

---
+++ console.py
@@ -0,0 +1,8 @@
+import models
+import cmd 
+
+
+class HBNBCommand(cmd.Cmd):
+    """ cmd clone"""
+    intro = 'Welcome to the Airbnb console. Type help or ? to list commands.\n'
+    prompt = '(hbnb) '
