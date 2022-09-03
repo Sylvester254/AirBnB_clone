4cc8904b2bcaff958be30d4bf96b62f16ae61c9f
Sylvester254 <slynganga59@gmail.com>
2022-09-03 15:33:44 +0300

    do update

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import re
 from models.base_model import BaseModel
 from models import storage
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -98,7 +99,53 @@ class HBNBCommand(cmd.Cmd):
         else:
             l = [str(obj) for key, obj in storage.all().items()]
             print(l)
-        
-        
+    def do_update(self, input):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute
+        """
+        if input == "" or input is None:
+            print("** class name missing **")
+            return
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, input)
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                attributes = storage.attributes()[classname]
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+                elif cast:
+                    try:
+                        value = cast(value)
+                    except ValueError:
+                        pass  # fine, stay a string then
+                setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
